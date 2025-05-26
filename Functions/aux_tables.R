e2f1191d83f238e4f0784b4589880b3fbba25350
Caterina Gregorio <caterina.gregorio@age.aldrecentrum.local>
2025-05-26 15:48:42 +0000

    Tables for the result of the simulation

--- Functions/aux_tables.R
+++ Functions/aux_tables.R
@@ -1,10 +1,10 @@
 fix_table <- function(pm_data){
   table <- split(pm_data, pm_data$trans)
   table <- lapply(table, function(x)
-    x %>% dplyr::select(parameter, model, estimate, bias, coverage))
+    x %>% dplyr::select(parameter, model, estimate, bias,se_bias, coverage))
   
   table <- lapply(table, function(x)
-    x %>% mutate_at(3:4, round, 3) %>% mutate_at(5, round, 2)  %>% 
+    x %>% mutate_at(3:5, round, 3) %>% mutate_at(6, round, 2)  %>% 
       mutate_all(as.character) %>% 
       filter(parameter%in%c("beta_cov1","beta_cov2","beta_cov3")))
   
@@ -12,10 +12,10 @@ fix_table <- function(pm_data){
     res <- split(table[[y]],table[[y]]$parameter)
     tab <- do.call("rbind",lapply(1:3, function(x){
       res[[x]] %<>% dplyr::select(-parameter)
-      res2 <- rbind(rep(paste0("**beta_",x,"**"),4),lab,res[[x]] %>% slice(c(5,2,1,3,4)))
+      res2 <- rbind(rep(paste0("**beta_",x,"**"),5),res[[x]] %>% slice(c(5,2,1,4,3)))
       return(res2)
     }))
-    tab <- rbind(rep(lab_trans[y],4),tab)
+    tab <- cbind(tab,trans=rep(y,nrow(tab)))
   }
   
   ))
